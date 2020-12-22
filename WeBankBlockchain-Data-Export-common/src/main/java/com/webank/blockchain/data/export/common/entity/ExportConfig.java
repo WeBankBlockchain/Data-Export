5b31228f6081fcc1d6992c7049941a36a7859027
卡拉马佐夫 <973459080@qq.com>
2020-12-22 15:23:26 +0800

    export param update

--- WeBankBlockchain-Data-Export-common/src/main/java/com/webank/blockchain/data/export/common/entity/ExportConfig.java
+++ WeBankBlockchain-Data-Export-common/src/main/java/com/webank/blockchain/data/export/common/entity/ExportConfig.java
@@ -21,6 +21,6 @@ public class ExportConfig {
     private String zookeeperNamespace;
     private String prepareTaskJobCron = "0/"+ frequency + " * * * * ?";
     private String dataFlowJobCron = "0/"+ frequency + " * * * * ?";
-    private String dataFlowJobItemParameters;
+    private String dataFlowJobItemParameters = "0=A,1=B,2=C,3=D,4=E,5=F,6=G,7=H";
     private int dataFlowJobShardingTotalCount = 8;
 }
