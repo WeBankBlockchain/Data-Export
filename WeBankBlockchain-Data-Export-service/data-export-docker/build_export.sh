1a9b707d5e0911a7419c26a8f969567be9cedca5
卡拉马佐夫 <973459080@qq.com>
2021-04-08 17:44:04 +0800

    1.7.2-beta

--- WeBankBlockchain-Data-Export-service/data-export-docker/build_export.sh
+++ WeBankBlockchain-Data-Export-service/data-export-docker/build_export.sh
@@ -60,7 +60,7 @@ fi
 
 
 docker pull wangyue168git/dataexport:1.7.2
-docker run -d -p 5200:5200  -v "$BASE_DIR"/config/:/config -v "$BASE_DIR"/log/:/log --name dataexport wangyue168git/dataexport:1.7.2
+docker run -d -p 5200:5200  -v "$BASE_DIR"/config/:/config -v "$BASE_DIR"/log/:/log --name dataexport fiscodev/dataexport:1.7.2
 if [ $? -ne 0 ]; then
     echo "data export run failed"
 else
